e85d92eb8351c56068bd2c7814892e934dc19c94
skohler00 <skohler00@gmail.com>
2018-12-07 12:55:16 -0500

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Susan Kohler on 12/7/18.
+//  Copyright © 2018 Susan Kohler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
